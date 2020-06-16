f10cad862d637cc0f78dc6b8b9c636829b205f74
Yuri <yurioliveira@aluno.fapce.edu.br>
2020-06-16 13:56:12 -0300

    style: Estilização da pagina Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,95 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRgXYY93LRaViuO0CxQMTBcwltSC7blEpwGL6KM-oa7Ss6AlxU&usqp=CAE" alt="Tênis" />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRgXYY93LRaViuO0CxQMTBcwltSC7blEpwGL6KM-oa7Ss6AlxU&usqp=CAE" alt="Tênis" />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRgXYY93LRaViuO0CxQMTBcwltSC7blEpwGL6KM-oa7Ss6AlxU&usqp=CAE" alt="Tênis" />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRgXYY93LRaViuO0CxQMTBcwltSC7blEpwGL6KM-oa7Ss6AlxU&usqp=CAE" alt="Tênis" />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRgXYY93LRaViuO0CxQMTBcwltSC7blEpwGL6KM-oa7Ss6AlxU&usqp=CAE" alt="Tênis" />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRgXYY93LRaViuO0CxQMTBcwltSC7blEpwGL6KM-oa7Ss6AlxU&usqp=CAE" alt="Tênis" />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
