b1ef2ee7e789406cde7d3c2ec09b8bbacc17727f
Yuri <yurioliveira@aluno.fapce.edu.br>
2020-06-17 11:13:18 -0300

    feat: Busca dados da API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,95 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRgXYY93LRaViuO0CxQMTBcwltSC7blEpwGL6KM-oa7Ss6AlxU&usqp=CAE" alt="Tênis" />
 
-        <strong>Tênis muito legal</strong>
-        <span>R$129,90</span>
-        
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRgXYY93LRaViuO0CxQMTBcwltSC7blEpwGL6KM-oa7Ss6AlxU&usqp=CAE" alt="Tênis" />
+  async componentDidMount() {
+    const response = await api.get('products');
 
-        <strong>Tênis muito legal</strong>
-        <span>R$129,90</span>
-        
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRgXYY93LRaViuO0CxQMTBcwltSC7blEpwGL6KM-oa7Ss6AlxU&usqp=CAE" alt="Tênis" />
+    this.setState({ products: data }); 
+  }
 
-        <strong>Tênis muito legal</strong>
-        <span>R$129,90</span>
-        
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+  render() {
+    const { products } = this.state;
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRgXYY93LRaViuO0CxQMTBcwltSC7blEpwGL6KM-oa7Ss6AlxU&usqp=CAE" alt="Tênis" />
-
-        <strong>Tênis muito legal</strong>
-        <span>R$129,90</span>
-        
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRgXYY93LRaViuO0CxQMTBcwltSC7blEpwGL6KM-oa7Ss6AlxU&usqp=CAE" alt="Tênis" />
-
-        <strong>Tênis muito legal</strong>
-        <span>R$129,90</span>
-        
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRgXYY93LRaViuO0CxQMTBcwltSC7blEpwGL6KM-oa7Ss6AlxU&usqp=CAE" alt="Tênis" />
-
-        <strong>Tênis muito legal</strong>
-        <span>R$129,90</span>
-        
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+    return (
+      <ProductList>
+        { products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+  
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
+          
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#fff" /> 3
+            </div>
+  
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+        )) }
+      </ProductList>
+    );
+  }
 }
