7a5d39504d46fbf550b7a169be4518e864a9a22b
Yuri <yurioliveira@aluno.fapce.edu.br>
2020-06-19 15:24:47 -0300

    chore: Configurando o Redux Saga

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -25,10 +25,10 @@ import { ProductList } from './styles';
       this.setState({ products: data }); 
     }
 
-    handleAddProduct = product => {
-      const { addToCart } = this.props;
+    handleAddProduct = id => {
+      const { addToCartRequest } = this.props;
 
-      addToCart(product);
+      addToCartRequest(id);
     };
 
     render() {
@@ -46,7 +46,7 @@ import { ProductList } from './styles';
             
             <button 
               type="button" 
-              onClick={() => this.handleAddProduct(product)}
+              onClick={() => this.handleAddProduct(product.id)}
             >
               <div>
                 <MdAddShoppingCart size={16} color="#fff" />{' '} {amount[product.id] || 0}
